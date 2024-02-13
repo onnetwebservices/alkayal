b16f8485d4aed5cb10347ff6edb7e6383aee3d41
jisha thomas <jishathomas88@gmail.com>
2024-02-13 14:17:00 +0400

    change src in certf

--- src/components/Certif.jsx
+++ src/components/Certif.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import pdf from '../assets/alkayalcertificate.pdf'
 
 function Certif() {
      
@@ -8,7 +9,7 @@ function Certif() {
         <h1 className='bgText fw-bold'>Certificates</h1>
       </div>
     <div className='col-lg-11 col-md-11 col-sm-11 mx-auto ' style={{height:'85vh'}} >
-      <embed src='../assets/files/alkayalcertificate.pdf#toolbar=0' width='100%' height='100%' type='application/pdf'  />
+      <embed src={`${pdf}#toolbar=0`} width='100%' height='100%' type='application/pdf'  />
     </div>
     </div>
   )

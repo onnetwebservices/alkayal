1ef62d4b23c31f407f60a931141340d555b74c70
jisha thomas <jishathomas88@gmail.com>
2024-02-13 10:20:03 +0400

    Updates

---
+++ assets/Service-28mlhyNN.js
@@ -0,0 +1,1 @@
+import{j as e,S as r}from"./index-Flrt5_6Q.js";function t(){return e.jsx("div",{children:e.jsx(r,{})})}export{t as default};
--- assets/Service-31_ZYRXm.js
+++
@@ -1,1 +0,0 @@
-import{j as e,S as r}from"./index-a789HGfS.js";function t(){return e.jsx("div",{children:e.jsx(r,{})})}export{t as default};
